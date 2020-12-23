4f80d8c3b3e372e207d45c51c4b172888f810914
kicok <kicoker@gmail.com>
2020-12-23 16:08:07 +0900

    Building and Styling the Accordion

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
 import React from 'react';
+import Accoridon from './components/Accordion';
+
+const items = [
+  {
+    title: 'What is React?',
+    content: 'React is a front end javascript framework',
+  },
+  {
+    title: 'Why use React?',
+    content: 'React is a favorite JS library amiong engineers',
+  },
+  {
+    title: 'How do you use React?',
+    content: 'You use React by creating components',
+  },
+];
 
 export default () => {
-  return <h1>Widget app</h1>;
+  return (
+    <div>
+      <Accoridon items={items} />
+    </div>
+  );
 };
