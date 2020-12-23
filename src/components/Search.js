4e65bf860bc92ee7be5e99bf6501bb95b016cc05
kicok <kicoker@gmail.com>
2020-12-23 18:42:15 +0900

    add useEffect

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Search = () => {
-  return <div> Search</div>;
+  const [term, setTerm] = useState('');
+  console.log('I RUN WITH EVERY RENDER');
+  useEffect(() => {
+    console.log('I only run once');
+  }, []);
+  // []      : Run at initial render
+  // nothing : Run at initial render, Run after every rerender
+  // [data]  : Run at initial render, Run after every rerender if data has changed since last render
+
+  return (
+    <div className="ui form">
+      <div className="field">
+        <label>Enter Search Term</label>
+        <input
+          value={term}
+          onChange={(e) => setTerm(e.target.value)}
+          className="input"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Search;
