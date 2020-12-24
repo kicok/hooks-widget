c7afce39e6a3f017ab3a6a713ede9cb390ede454
kicok <kicoker@gmail.com>
2020-12-24 15:38:53 +0900

    add Dropdown

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Accordion from './components/Accordion';
+import Dropdown from './components/Dropdown';
 import Search from './components/Search';
 
 const items = [
@@ -17,10 +18,30 @@ const items = [
   },
 ];
 
+const options = [
+  {
+    label: 'The color Red',
+    value: 'red',
+  },
+  {
+    label: 'The Color Green',
+    value: 'green',
+  },
+  {
+    label: 'A Shade of Blue',
+    value: 'blue',
+  },
+];
+
 export default () => {
+  const [selected, setSelected] = useState(options[0]);
   return (
     <div>
-      <Search />
+      <Dropdown
+        selected={selected}
+        options={options}
+        onSelectedChange={setSelected}
+      />
     </div>
   );
 };
